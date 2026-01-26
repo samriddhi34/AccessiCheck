815dcb0b1ba2cfe8e28f379e7dd880b22edda073
Samriddhi Sinha <samriddhisinha52@gmail.com>
2026-01-26 22:55:29 +0530

    Adding a backend API endpoint that receives a URL and returns it

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,21 @@
 
-from flask import Flask , jsonify
+from flask import Flask , jsonify , request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return jsonify({"message" : "Accessible Backend Running"})
+
+
+@app.route('/submit-url' , methods=["POST"])
+def submit_url():
+    data = request.get_json()
+
+    return jsonify({
+        "status" : "received",
+        "url" : data["url"]
+    })
 
 if __name__ == "__main__":
     app.run(debug= True)
